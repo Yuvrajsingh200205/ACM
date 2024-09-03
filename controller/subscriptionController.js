a89fc20e1493df78fc0583125044e134b9e48946
Yuvraj singh <yuvrajsingh200205@gmail.com>
2024-09-03 14:08:38 +0530

    correction

--- controller/subscriptionController.js
+++ controller/subscriptionController.js
@@ -13,11 +13,14 @@ async function subscribe(email) {
     await sendThankYouEmail(email);
 
     return { success: true, message: 'Subscription successful.' };
-  } catch (error) {
-    if (error.code === '23505') { // unique_violation
-      res.status(409).json({ message: 'Email already subscribed' });
+  } catch (err) {
+    console.error('Error during subscription', err);
+
+    // Handle duplicate email error
+    if (err.code === '23505') { // Unique violation error code in PostgreSQL
+      return { success: false, message: 'Email already subscribed.' };
     } else {
-      res.status(500).json({ message: 'Internal server error' });
+      return { success: false, message: 'Server error occurred during subscription.' };
     }
   }
 }
@@ -25,40 +28,3 @@ async function subscribe(email) {
 module.exports = { subscribe };
 
 
-// const pool = require('../db/connection');
-// const { sendThankYouEmail } = require('./emailController');
-
-// async function subscribe(email) {
-//   try {
-
-//     // Insert the email into the database
-//     await pool.query('INSERT INTO SUBSCRIBERS (email) VALUES ($1)', [email]);
-
-//     // Send the thank-you email
-//     await sendThankYouEmail(email);
-
-//     const checkQuery = 'SELECT * FROM SUBSCRIBERS WHERE email = $1';
-//     const checkResult = await pool.query(checkQuery, [email]);
-
-//     if (checkResult.rows.length > 0) {
-//       return { success: false, message: 'Email is already subscribed.' };
-//     }
-
-//     const insertQuery = 'INSERT INTO SUBSCRIBERS (email) VALUES ($1)';
-//     await pool.query(insertQuery, [email]);
-
-//     return { success: true, message: 'Subscription successful.' };
-//   } catch (err) {
-//     console.error('Error during subscription', err);
-
-//     if (err.code === '23505') { 
-//       res.status(409).json({ message: 'Email already subscribed' });
-//     } else {
-//       res.status(500).json({ message: 'Server error' });
-//     }
-
-//     return { success: false, message: 'An error occurred during subscription.' };
-//   }
-// }
-
-// module.exports = { subscribe };
